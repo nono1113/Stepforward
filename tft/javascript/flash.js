581793f90f536ffb02937772677578dd59d1ed37
Miho Hamanaka <2081050.nono@gmail.com>
2020-09-15 23:18:26 +0900

    [old-tft_file]本番からダウンロード

---
+++ tft/javascript/flash.js
@@ -0,0 +1,14 @@
+//ƒtƒ‰ƒbƒVƒ…“Ç‚İ‚İ
+
+document.write("<object classid='clsid:D27CDB6E-AE6D-11cf-96B8-444553540000'")
+document.write(" codebase='http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,19,0")
+document.write("flash/swflash.cab'")
+document.write(" width=816 height=260>")
+document.write("<param name=movie value='pic.swf'>")
+document.write("<param name=quality value=high>")
+document.write("<embed src='pic.swf' quality=high  ")
+document.write(" width=816 height=260")
+document.write(" type='application/x-shockwave-flash' ")
+document.write("pluginspage='http://www.macromedia.com/go/getflashplayer'>")
+document.write("</embed>")
+document.write("</object>")
